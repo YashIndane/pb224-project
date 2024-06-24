9ae9e3effbf30f8edecc6f43edf59c6833184ec3
Yash Indane <yashindane46@gmail.com>
2024-06-25 01:23:59 +0530

    address shifting working

--- assembler/main.py
+++ assembler/main.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 
 from shifter import DigitalPin, Shifter, Hex
+import threading
 import RPi.GPIO as GPIO
+
+
+# Writes data to RAM at given address
+def write_single_record(ram_address: str, ram_data: str) -> None:
+
+    # Shifting address and data
+    address_shifter_thread = threading.Thread(
+        target=address_shifter.shift,
+        kwargs={
+            'shiftHex': Hex(hexString=ram_address)
+        }
+    )
+
+    data_shifter_thread = threading.Thread(
+        target=data_shifter.shift,
+        kwargs={
+            'shiftHex': Hex(hexString=ram_data)
+        }
+    )
+
+    address_shifter_thread.start()
+    data_shifter_thread.start()
+    address_shifter_thread.join()
+    data_shifter_thread.join()
+
+    print("Data written!!")
 
 
 if __name__ == "__main__":
@@ -9,7 +36,7 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-    addrdata = "0x023c:0x100001"
+    addrdata = "0x00bb:0x7700e9"
     addr, data = addrdata.split(":")
 
     DATA_SER   = DigitalPin(pinNo=23, mode=GPIO.OUT, initialValue=0)
@@ -17,13 +44,17 @@ if __name__ == "__main__":
     DATA_RCLK  = DigitalPin(pinNo=15, mode=GPIO.OUT, initialValue=0)
     DATA_SRCLR = DigitalPin(pinNo=14, mode=GPIO.OUT, initialValue=1)
 
+    data_shifter = Shifter(shifterDigitalPins=[DATA_SER, DATA_SRCLK, DATA_RCLK, DATA_SRCLR], shifterDelay=0.05)
+    data_shifter.clear_register()
 
-    address_shifter = Shifter(shifterDigitalPins=[DATA_SER, DATA_SRCLK, DATA_RCLK, DATA_SRCLR], shifterDelay=0.05)
+    ADDR_SER   = DigitalPin(pinNo=26, mode=GPIO.OUT, initialValue=0)
+    ADDR_SRCLK = DigitalPin(pinNo=22, mode=GPIO.OUT, initialValue=0)
+    ADDR_RCLK  = DigitalPin(pinNo=19, mode=GPIO.OUT, initialValue=0)
+    ADDR_SRCLR = DigitalPin(pinNo=6, mode=GPIO.OUT, initialValue=1)
+
+    address_shifter = Shifter(shifterDigitalPins=[ADDR_SER, ADDR_SRCLK, ADDR_RCLK, ADDR_SRCLR], shifterDelay=0.05)
     address_shifter.clear_register()
 
-    address_shifter.shift(
-        shiftHex = Hex(hexString=data)
-    )
+    write_single_record(ram_address=addr, ram_data=data)
 
     GPIO.cleanup()
-
