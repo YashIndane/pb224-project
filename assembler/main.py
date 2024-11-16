f8a2515d7b74fada4312bf64cc80fdabccd35b16
Yash Indane <yashindane46@gmail.com>
2024-11-16 21:33:36 +0530

    config parser functionality  (#3)
    Added config file functionality, to parse config pin profiles and
    configure GPIO pins accordingly.

--- assembler/main.py
+++ assembler/main.py
@@ -1,75 +1,38 @@
 #!/usr/bin/python3
 
-from shifter import Shifter
-from digitalpin import DigitalPin
-import ram_operations
 import RPi.GPIO as GPIO
+import config_parser
+from pathlib import Path
 import time
 
 
 if __name__ == "__main__":
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+    path = Path(__file__).parent / "../pb224_config.yaml"
+    ram_OP = config_parser.parse_config(conf_file=path)
 
-    # addrdata = "0x810d:0x6700bc"
-    # addr, data = addrdata.split(":")
+    ram_OP.write_single_address(hex_address="0x1968", hex_data="0x37cca2")
+    time.sleep(0.05)
+    ram_OP.write_single_address(hex_address="0x1974", hex_data="0xc28155")
+    time.sleep(0.05)
+    ram_OP.write_single_address(hex_address="0x1999", hex_data="0xe80065")
+    time.sleep(0.05)
+    ram_OP.write_single_address(hex_address="0x2006", hex_data="0xa1b9d3")
+    time.sleep(0.05)
+    ram_OP.write_single_address(hex_address="0x1618", hex_data="0xf62011")
 
-    DATA_SER   = DigitalPin(pinNo=23, mode=GPIO.OUT, initialValue=0)
-    DATA_SRCLK = DigitalPin(pinNo=24, mode=GPIO.OUT, initialValue=0)
-    DATA_RCLK  = DigitalPin(pinNo=15, mode=GPIO.OUT, initialValue=0)
-    DATA_SRCLR = DigitalPin(pinNo=16, mode=GPIO.OUT, initialValue=1)
+    time.sleep(0.05)
+    print(ram_OP.read_single_address(hex_address="0x1968"))
+    time.sleep(0.05)
+    print(ram_OP.read_single_address(hex_address="0x1974"))
+    time.sleep(0.05)
+    print(ram_OP.read_single_address(hex_address="0x1999"))
+    time.sleep(0.05)
+    print(ram_OP.read_single_address(hex_address="0x2006"))
+    time.sleep(0.05)
+    print(ram_OP.read_single_address(hex_address="0x1618"))
 
-    data_shifter = Shifter(shifterDigitalPins=[DATA_SER, DATA_SRCLK, DATA_RCLK, DATA_SRCLR], shifterDelay=0.05)
-    data_shifter.clear_register()
-
-    ADDR_SER   = DigitalPin(pinNo=17, mode=GPIO.OUT, initialValue=0)
-    ADDR_SRCLK = DigitalPin(pinNo=27, mode=GPIO.OUT, initialValue=0)
-    ADDR_RCLK  = DigitalPin(pinNo=22, mode=GPIO.OUT, initialValue=0)
-    ADDR_SRCLR = DigitalPin(pinNo=5, mode=GPIO.OUT, initialValue=1)
-
-    address_shifter = Shifter(shifterDigitalPins=[ADDR_SER, ADDR_SRCLK, ADDR_RCLK, ADDR_SRCLR], shifterDelay=0.05)
-    address_shifter.clear_register()
-
-    # RAM write pins
-    RI     = DigitalPin(pinNo=6, mode=GPIO.OUT, initialValue=0)
-    RI_CLK = DigitalPin(pinNo=26, mode=GPIO.OUT, initialValue=0)
-
-    # RAM Read pins
-    LATCH       = DigitalPin(pinNo=13, mode=GPIO.OUT, initialValue=1)
-    SHIFT_CLK   = DigitalPin(pinNo=4, mode=GPIO.OUT, initialValue=0)
-    SER_DATA_IN = DigitalPin(pinNo=12, mode=GPIO.IN)
-
-    ram_OP = ram_operations.RAM_Interface(
-        R_Pins=[LATCH, SHIFT_CLK, SER_DATA_IN],
-        W_Pins=[RI, RI_CLK],
-        addr_shifter=address_shifter,
-        data_shifter=data_shifter
-    )
-
-    #ram_OP.write_single_address(hex_address="0x0006", hex_data="0x939393")
-    #time.sleep(0.05)
-    #ram_OP.write_single_address(hex_address="0x0007", hex_data="0x19ac76")
-    #time.sleep(0.05)
-    #ram_OP.write_single_address(hex_address="0x0008", hex_data="0xc7bbb2")
-    #time.sleep(0.05)
-    #ram_OP.write_single_address(hex_address="0x0009", hex_data="0x743aaa")
-    #time.sleep(0.05)
-    #ram_OP.write_single_address(hex_address="0x000a", hex_data="0x0092a1")
-
-    #time.sleep(0.05)
-    #print(ram_OP.read_single_address(hex_address="0x0001"))
-    #time.sleep(0.05)
-    #print(ram_OP.read_single_address(hex_address="0x0002"))
-    #time.sleep(0.05)
-    #print(ram_OP.read_single_address(hex_address="0x0003"))
-    #time.sleep(0.05)
-    #print(ram_OP.read_single_address(hex_address="0x0004"))
-    #time.sleep(0.05)
-    #print(ram_OP.read_single_address(hex_address="0x0004"))
-
-    # Clear registers
-    data_shifter.clear_register()
-    address_shifter.clear_register()
+    ram_OP.clear_addr_reg()
+    ram_OP.clear_data_reg()
 
     GPIO.cleanup()
--- assembler/ram_operations.py
+++ assembler/ram_operations.py
@@ -30,7 +30,7 @@ class RAM_Interface:
         RI.set_value(value=0)
 
         # RI_CLK disabled
-        #RI_CLK.set_value(value=0)
+        # RI_CLK.set_value(value=0)
 
 
         # Set address
@@ -96,3 +96,11 @@ class RAM_Interface:
         time.sleep(0.05)
 
         print("data written")
+
+
+    def clear_addr_reg(self) -> None:
+        self.addr_shifter.clear_register()
+
+
+    def clear_data_reg(self) -> None:
+        self.data_shifter.clear_register()
